5b467e0c86885bda6d533f17cb49421a3fea9af3
wills-dev <ejioguvictor6@gmail.com>
2025-01-03 15:02:00 +0100

    added: subscription

---
+++ app/dashboard/subscription/overview/page.tsx
@@ -0,0 +1,13 @@
+import DashboardNav from "@/components/DashboardComponents/DashboardNav";
+import Overview from "@/components/SubscriptionComponent/Overview";
+
+const Subscription = () => {
+  return (
+    <main className="relative">
+      <DashboardNav />
+      <Overview />
+    </main>
+  );
+};
+
+export default Subscription;
---
+++ components/SubscriptionComponent/SubscriptionToggle.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+type Props = {
+  currentLink: number;
+};
+
+const subscriptionLinks = [
+  {
+    id: 1,
+    name: "overview",
+    link: "/dashboard/subscription/overview",
+  },
+  {
+    id: 2,
+    name: "plans",
+    link: "/dashboard/subscription/plans",
+  },
+];
+
+const SubscriptionToggle = ({ currentLink }: Props) => {
+  return (
+    <div className="w-full">
+      {" "}
+      <h2 className="text-xl font-bold max-sm:text-lg">Subscriptions</h2>
+      <ul className="flex items-center gap-4  mt-[1rem]">
+        {subscriptionLinks.map((link) => (
+          <Link
+            href={link.link}
+            key={link?.id}
+            className={`${
+              currentLink === link.id
+                ? "text-primary-green  border-b-primary-green border-b-2"
+                : "text-[#737774]"
+            } font-semibold capitalize`}
+          >
+            <li>{link.name}</li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SubscriptionToggle;
