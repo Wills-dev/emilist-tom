79451c96cd6aaaf01796e7821fa78e74928aabf7
wills-dev <ejioguvictor6@gmail.com>
2025-01-26 18:53:18 +0100

    update

--- hooks/useGetUser.ts
+++ hooks/useGetUser.ts
@@ -1,5 +1,6 @@
+import { useState } from "react";
+
 import { axiosInstance } from "@/axiosInstance/baseUrls";
-import React, { useState } from "react";
 
 export const useGetUser = () => {
   const [user, setUser] = useState<any>({});
@@ -9,7 +10,6 @@ export const useGetUser = () => {
     setIsLoading(true);
     try {
       const { data } = await axiosInstance.get(`/auth/user-details/${userId}`);
-      console.log("data", data);
       setUser(data?.data);
     } catch (error) {
       console.log("error getting user info", error);
